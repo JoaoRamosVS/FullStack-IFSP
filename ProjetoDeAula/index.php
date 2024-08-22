8ff2ff980852886d74af039f4b5997bffdac7d91
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-08-21 22:11:06 -0300

    Aula 01 - Granularização de projeto

---
+++ ProjetoDeAula/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>São Paulo FC</title>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">   
+  <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <?php include('includes/component/navbar.php') ?>
+  <?php include('includes/component/sidebar.php') ?> 
+  
+  <div class="content-wrapper">
+  	<?php include('includes/component/breadcrumb.php') ?> 
+    <section class="content">
+      <?php include('includes/component/default-box.php') ?>
+    </section>
+  </div>
+  <?php include('includes/component/footer.php') ?>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="adminlte/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="adminlte/dist/js/demo.js"></script>
+</body>
+</html>
